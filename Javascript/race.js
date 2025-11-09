e90ccaa30becf955257ec11b7c402dcb7e93efd5
Ruswole <norlydude@gmail.com>
2025-11-09 14:46:38 +0800

    Write functionality sample race

---
+++ Javascript/race.js
@@ -0,0 +1,16 @@
+function countdown() {
+  console.log(5);
+  console.log(4);
+  console.log(3);
+  console.log(2);
+  console.log(1);
+}
+
+// Setting up the race
+
+countdown();
+// Go!
+// Players are running the race
+// Race is finished!
+
+// Get ready for new race
