e0990a720af1e4a33c961c54ccf19a8dbb2b240b
Ruswole <norlydude@gmail.com>
2025-11-09 23:58:19 +0800

    configuration of index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -33,6 +33,18 @@
 // bonusPoints = bonusPoints + 45;
 // console.log(bonusPoints);
 
+// function increment() {
+//   console.log("The button was clicked");
+// }
+
+let countEl = document.getElementById("count-el");
+
+console.log(countEl);
+
+let count = 0;
+
 function increment() {
-  console.log("The button was clicked");
+  count = count + 1;
+
+  console.log(count);
 }
