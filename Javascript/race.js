9ccae6d8593196df605efda4e67f2a943140f57e
Ruswole <norlydude@gmail.com>
2025-11-09 14:58:30 +0800

    Write functions that increments

--- Javascript/race.js
+++ Javascript/race.js
@@ -1,16 +1,37 @@
-function countdown() {
-  console.log(5);
-  console.log(4);
-  console.log(3);
-  console.log(2);
-  console.log(1);
-}
+// function countdown() {
+//   console.log(5);
+//   console.log(4);
+//   console.log(3);
+//   console.log(2);
+//   console.log(1);
+// }
 
-// Setting up the race
+// // Setting up the race
 
-countdown();
+// countdown();
 // Go!
 // Players are running the race
 // Race is finished!
 
 // Get ready for new race
+
+// function myLogger() {
+//   console.log(42);
+// }
+
+// myLogger();
+
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+
+// function logLapTime() {
+//   let totalTime = lap1 + lap2 + lap3;
+//   console.log(logLapTime);
+// }
+
+// logLapTime();
+
+let LapsCompleted = 0;
+
+console.log(LapsCompleted);
